1502dd24b68b222c716367c68442153ddccffa95
Catia Barroco <104831678+CatiaBarroco-xgeeks@users.noreply.github.com>
2023-05-03 15:09:02 +0100

    link navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image"
-import { GetStaticProps} from "next"
+import { GetStaticProps } from "next"
+import Link from "next/link"
 
 import { useKeenSlider } from 'keen-slider/react'
 
 import { stripe } from "../lib/stripe"
 import { HomeContainer, Product } from "../styles/pages/home"
-
 import 'keen-slider/keen-slider.min.css'
 import Stripe from "stripe"
-
 interface HomeProps {
     products: {
         id: string
@@ -17,7 +16,6 @@ interface HomeProps {
         price: number
     }[]
 }
-
 export default function Home({ products }: HomeProps) {
     const [sliderRef] = useKeenSlider({
         slides: {
@@ -25,25 +23,27 @@ export default function Home({ products }: HomeProps) {
             spacing: 48
         }
     });
-
     return (
         <HomeContainer ref={sliderRef} className="keen-slider">
             {products.map(product => {
                 return (
-                    <Product key={product.id} className="keen-slider__slide">
-                        <Image src={product.imageUrl} width={520} height={480} alt="" />
+                    <Link href={`/product/${product.id}`} key={product.id}>
+                        <Product
+                            className="keen-slider__slide"
+                        >
+                            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-                        <footer>
-                            <strong>{product.name}</strong>
-                            <span>{product.price}</span>
-                        </footer>
-                    </Product>
+                            <footer>
+                                <strong>{product.name}</strong>
+                                <span>{product.price}</span>
+                            </footer>
+                        </Product>
+                    </Link>
                 )
             })}
         </HomeContainer>
     )
 }
-
 export const getStaticProps: GetStaticProps = async () => {
     const response = await stripe.products.list({
         expand: ['data.default_price']
@@ -54,9 +54,9 @@ export const getStaticProps: GetStaticProps = async () => {
             id: product.id,
             name: product.name,
             imageUrl: product.images[0],
-            price: new Intl.NumberFormat('US', {
+            price: new Intl.NumberFormat('pt-BR', {
                 style: 'currency',
-                currency: 'EUR'
+                currency: 'BRL'
             }).format(price.unit_amount / 100),
         }
     })
