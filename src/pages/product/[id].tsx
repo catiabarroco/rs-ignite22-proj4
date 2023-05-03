8a7b15bb26eef9c47129e1e489cb044af0cebcc9
Catia Barroco <104831678+CatiaBarroco-xgeeks@users.noreply.github.com>
2023-05-03 15:42:49 +0100

    buy product handle button

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -11,10 +11,14 @@ interface ProductProps {
         imageUrl: string
         price: string
         description: string
+        defaultPriceId: string
     }
 }
 
 export default function Product({ product }: ProductProps) {
+    function handleBuyButton() {
+        console.log(product.defaultPriceId);
+    }
     return (
         <ProductContainer>
             <ImageContainer>
@@ -27,7 +31,7 @@ export default function Product({ product }: ProductProps) {
 
                 <p>{product.description}</p>
 
-                <button>
+                <button onClick={handleBuyButton}>
                     Comprar agora
                 </button>
             </ProductDetails>
@@ -61,7 +65,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
                     style: 'currency',
                     currency: 'EUR'
                 }).format(price.unit_amount / 100),
-                description: product.description
+                description: product.description,
+                defaultPriceId: price.id
             }
         },
         revalidate: 60 * 60 * 1 // 1 hours
